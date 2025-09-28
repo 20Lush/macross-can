1096eac23cfdfe03f10584b0428abdad89576338
20Lush <zachfsx83@gmail.com>
2025-09-27 19:20:25 -0700

    Linting

--- src/main.c
+++ src/main.c
@@ -1,20 +1,22 @@
 #include "main.h"
-#include <Arduino.h>
+#include "core_pins.h"
 
 const int pin_led = LED_BUILTIN;
+
+#define DELAY_INTERVAL 250
 
 // This function runs once at startup.
 void setup()
 {
-	pinMode(pin_led, OUTPUT);
+   pinMode(pin_led, OUTPUT);
    digitalWriteFast(pin_led, HIGH);
 }
 
 // This function runs repeatedly after setup().
 void loop()
 {
-	digitalWriteFast(pin_led, HIGH);
-	delay(100);
-	digitalWriteFast(pin_led, LOW);
-	delay(100);
+   digitalWriteFast(pin_led, HIGH);
+   delay(DELAY_INTERVAL);
+   digitalWriteFast(pin_led, LOW);
+   delay(DELAY_INTERVAL);
 }
